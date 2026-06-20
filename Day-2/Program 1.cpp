506ee3fcd9a03cb4f9b4247e70335f6063e18cb2
deepikasharma919335-star <deepikasharma919335@gmail.com>
2026-06-20 23:37:48 +0530

    Write a program to find sum of digit of a number.

---
+++ Day-2/Program 1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    int n;
+    cout<<"enter a number";
+    cin>> n;
+    int sum=0;
+    while(n>0){
+        int digit=n%10;
+        sum+=digit;
+        n=n/10;
+
+    }
+    cout<<sum is <<sum<<" ";
+    
+    return 0;
+}
